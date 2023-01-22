e43079799718342b61560951065a078d8ca63491
emredal2 <emredaldesa@gmail.com>
2023-01-22 19:37:44 +0300

    Update and rename README.md to alan.js

---
+++ alan.js
@@ -0,0 +1,6 @@
+const yaricap = process.argv.slice(2)[0]*1
+function DaireAlaniHesapla() {
+    const daireAlani = Math.PI * (r*r)
+    console.log(`Yarıçapı (${yaricap}) olan dairenin alanı: (${daireAlani})'dir.`)
+}
+DaireAlaniHesapla()
