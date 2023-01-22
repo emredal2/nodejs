fc8e465f90492fba0a8ae07b813ddbc5e8c858b9
emredal2 <emredaldesa@gmail.com>
2023-01-22 19:49:15 +0300

    Update alan.js

--- alan.js
+++ alan.js
@@ -1,6 +1,6 @@
 const yaricap = process.argv.slice(2)[0]*1
 function DaireAlaniHesapla() {
-    const daireAlani = Math.PI * (r*r)
+    const daireAlani = Math.PI * Math.pow(yaricap,2)
     console.log(`Yarıçapı (${yaricap}) olan dairenin alanı: (${daireAlani})'dir.`)
 }
 DaireAlaniHesapla()
